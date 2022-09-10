29e93bd0daec8dc922326e43564cdf0a5b12daae
Kamil Preweda <preweda.kamil@o2.pl>
2022-09-10 18:58:41 +0200

    Got a logger

--- MonitoringApi/Controllers/UsersController.cs
+++ MonitoringApi/Controllers/UsersController.cs
@@ -8,6 +8,12 @@ namespace MonitoringApi.Controllers;
 [ApiController]
 public class UsersController : ControllerBase
 {
+    private readonly ILogger<UsersController> _logger;
+
+    public UsersController(ILogger<UsersController> logger)
+    {
+        _logger = logger;
+    }
     
     [HttpGet]
     public IEnumerable<string> Get()
